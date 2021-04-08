7c506739ade82be49c117689f7bd18182945435c
Stef van Esch <stef@marshmallow.dev>
2021-04-08 09:48:28 +0200

    NPM updates

--- resources/js/field.js
+++ resources/js/field.js
@@ -1,6 +1,6 @@
-import IndexField from '@/components/IndexField'
-import DetailField from '@/components/DetailField'
-import FormField from '@/components/FormField'
+import IndexField from '@/components/IndexField.vue'
+import DetailField from '@/components/DetailField.vue'
+import FormField from '@/components/FormField.vue'
 
 Nova.booting((Vue) => {
     Vue.component('index-advanced-image', IndexField)
